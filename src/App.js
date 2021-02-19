8524987ce75d6598f1269c25019b1d3009d98ccc
ayoub ar <ayoubarahmat9@gmail.com>
2021-02-19 18:15:22 +0100

    add aniamtion

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import Caroussel from './Components/Views/Caroussel';
 import Navbar from './Components/Views/Navbar';
 import Card from './Components/Views/Card';
 import Pagination from './Components/Views/Pagination';
-import poems  from "./Data/Data";
-import { List } from "./Components/Views/Card"
+import poems from './Data/Data';
+import { List } from './Components/Views/Card';
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
+
 // import ReactDOM from 'react-dom'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-
-  
 function App() {
+  const { width, height } = useWindowSize();
+
   return (
-    
-    <div className="container" >
-      <div class='row d-flex justify-content-around'>
-        {/* <i class="fab fa-facebook-square"></i> */}
-        <div  className=" logo">صباح بوعمر</div>
+    <>
+      <Confetti width={width} height={height} />
+      <div className="container">
+        <div className="row d-flex justify-content-around">
+          {/* <i className="fab fa-facebook-square"></i> */}
+          <div className=" logo">صباح بوعمر</div>
+        </div>
+        <Navbar />
+        {/* <Caroussel /> */}
+        <List datas={poems} />
+        <Pagination />
       </div>
-      <Navbar />
-      {/* <Caroussel /> */}
-      <List datas= {poems} />
-      <Pagination />
-    </div>
-    
-    
+    </>
   );
 }
 
--- src/Components/Views/Card.js
+++ src/Components/Views/Card.js
@@ -1,52 +1,93 @@
-import React , { useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import parse from 'html-react-parser';
+import FadeIn from 'react-fade-in';
 
-const customStyle = {
-    gridGap: '0px !important',
-    paddingTop:0,
-    paddingBottom:0
-}
-export const Card = props => {
-    const { data } = props
-    const [readMore, setReadMore] = useState(false)
+export const Card = (props) => {
+  const { data, toggleReadMore } = props;
 
-    const getthreeFirstLine = (text) => {
-        const str = text.split('\n')
-        const newStr = `
+  const getthreeFirstLine = (text) => {
+    const str = text.split('\n');
+    const newStr = `
             ${str[0]}
             ${str[1]}
             ${str[2]}
             ${str[3]}
-        `
-        return newStr
-    }
-    return (
-        <div>
-            <div className="card" >
-                <img className="card-img-top" 
-                    src={process.env.PUBLIC_URL + `/images/${data.image}`}/>
-                    <div className="card-body smooth-text">
-                        <h5 className="card-title">{data.title}</h5>
-                    <p className="card-text  text-right "> {!readMore && parse(`${getthreeFirstLine(data.text)}...`)} </p>
-                    <p className="card-text  text-right "> {readMore && parse(`${data.text}`)} </p>
-                    <a onClick={() => setReadMore(!readMore)} type="button" className="btn " id="read"> تابع القراءة</a>
-                        
-                    </div>
-            </div>
-        </div>
-        
-    )
+        `;
+    return newStr;
+  };
+
+  const onClickReadMore = () => {
+    toggleReadMore(data.id);
+  };
+
+  return (
+    <div>
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={process.env.PUBLIC_URL + `/images/${data.image}`}
+          alt="test"
+        />
+        <FadeIn>
+          <div className="card-body smooth-text">
+            <h5 className="card-title">{data.title}</h5>
+            <p className="card-text  text-right ">
+              {' '}
+              {!data.readMore && (
+                <FadeIn> {parse(`${getthreeFirstLine(data.text)}...`)} </FadeIn>
+              )}{' '}
+            </p>
+            <p className="card-text  text-right ">
+              {' '}
+              {data.readMore && <FadeIn>{parse(`${data.text}`)}</FadeIn>}
+            </p>
+            <a
+              onClick={onClickReadMore}
+              type="button"
+              className="btn "
+              id="read"
+            >
+              {' '}
+              تابع القراءة
+            </a>
+          </div>
+        </FadeIn>
+      </div>
+    </div>
+  );
 };
 
-export const List = props => {
-    const { datas } = props
+export const List = (props) => {
+  const { datas } = props;
+  const [data_, setData] = useState([]);
 
-    return (
-            <div className="grid-card">
-            {datas.map(data => <Card data={data}/>)}     
-            </div>
-        )}
+  useEffect(() => {
+    setData(props.datas.map((m) => ({ ...m, readMore: false })));
+  }, []);
 
+  const toggleReadMore = (id) => {
+    console.log('toggle', id);
+    let new_table = [];
+    for (let i in data_) {
+      if (datas[i].id === id) {
+        new_table.push({ ...datas[i], readMore: !data_[i].readMore });
+      } else {
+        new_table.push({ ...datas[i], readMore: false });
+      }
+    }
 
-export default Card
+    console.log(new_table);
+
+    setData(new_table);
+  };
+  return (
+    <div className="grid-card">
+      {data_.map((data) => (
+        <Card key={data.id} data={data} toggleReadMore={toggleReadMore} />
+      ))}
+    </div>
+  );
+};
+
+export default Card;
